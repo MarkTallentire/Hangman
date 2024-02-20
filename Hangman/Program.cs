defdeace53089d3c8e77b5cd37b02f60d1b5ebee
Mark Tallentire <mark.tallentire@hazcheck.com>
2024-02-20 08:59:04 +0000

    Refactor

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -22,19 +22,18 @@ public class HangmanGame
         Game();
     }
 
-
-    public void Game()
+    private void Game()
     {
-        var inputOutput = new InputOutput(_word, _guesses);
+        var inputOutput = new InputOutput();
         
         while (_numberOfGuesses <= 5)
         {
-            inputOutput.Render();
+            inputOutput.Render(_word, _guesses);
             
             var guess = inputOutput.GetGuess();
             _guesses.Add(guess);
             
-            inputOutput.Render();
+            inputOutput.Render(_word, _guesses);
             if (CheckIfWon())
             {
                 Console.WriteLine("You're winner!");
@@ -68,14 +67,6 @@ public class HangmanGame
 
 public class InputOutput
 {
-    private string _word;
-    private List<char> _guesses;
-    
-    public InputOutput(string word, List<char> guesses)
-    {
-        _word = word;
-        _guesses = guesses;
-    }
 
     public char GetGuess()
     {
@@ -83,17 +74,17 @@ public class InputOutput
         return Console.ReadKey(true).KeyChar;
     }
     
-    public void Render()
+    public void Render(string word, List<char> guesses)
     {
         Console.Clear();
-        for (int i = 0; i < _word.Length; i++)
+        for (int i = 0; i < word.Length; i++)
         {
-            if(_guesses.Contains(char.ToLower(_word[i])))
-                Console.Write(_word[i]);
+            if(guesses.Contains(char.ToLower(word[i])))
+                Console.Write(word[i]);
             else
                 Console.Write("_");
             
-            if(i != _word.Length - 1)
+            if(i != word.Length - 1)
                 Console.Write(" ");
         }
         
